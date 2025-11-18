d8d85b8010d9586adeb4b533120d5fe19fe0685f
TitasBal <t.balvocius@gmail.com>
2025-11-18 13:20:48 +0200

    Update dataset.py
    Update dataset.py to account for the subword tokenizer

--- dataset.py
+++ dataset.py
@@ -3,13 +3,8 @@ from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
 import os
 
-from vocabulary import tokenize_de, tokenize_en
-
 class TranslationDataset(Dataset):
-    """
-    A PyTorch Dataset for loading parallel text data.
-    """
-    def __init__(self, data_dir, de_vocab, en_vocab, mode='train'):
+    def __init__(self, data_dir, tokenizer, mode='train'):
         self.de_path = os.path.join(data_dir, f'{mode}.de')
         self.en_path = os.path.join(data_dir, f'{mode}.en')
         
@@ -18,8 +13,9 @@ class TranslationDataset(Dataset):
         with open(self.en_path, 'r', encoding='utf-8') as f:
             self.en_sents = f.read().strip().split('\n')
             
-        self.de_vocab = de_vocab
-        self.en_vocab = en_vocab
+        self.tokenizer = tokenizer
+        self.sos_id = tokenizer.token_to_id("<sos>")
+        self.eos_id = tokenizer.token_to_id("<eos>")
 
     def __len__(self):
         return len(self.de_sents)
@@ -28,18 +24,15 @@ class TranslationDataset(Dataset):
         de_sent = self.de_sents[idx]
         en_sent = self.en_sents[idx]
         
-        de_tokens = [self.de_vocab.stoi.get(t, self.de_vocab.stoi["<unk>"]) for t in tokenize_de(de_sent)]
-        en_tokens = [self.en_vocab.stoi.get(t, self.en_vocab.stoi["<unk>"]) for t in tokenize_en(en_sent)]
+        de_tokens = self.tokenizer.encode(de_sent).ids
+        en_tokens = self.tokenizer.encode(en_sent).ids
         
-        de_tensor = torch.tensor([self.de_vocab.stoi["<sos>"]] + de_tokens + [self.de_vocab.stoi["<eos>"]])
-        en_tensor = torch.tensor([self.en_vocab.stoi["<sos>"]] + en_tokens + [self.en_vocab.stoi["<eos>"]])
+        de_tensor = torch.tensor([self.sos_id] + de_tokens + [self.eos_id])
+        en_tensor = torch.tensor([self.sos_id] + en_tokens + [self.eos_id])
         
         return en_tensor, de_tensor
 
 def get_collate_fn(pad_idx):
-    """
-    Returns a collate_fn function that pads sequences in a batch.
-    """
     def collate_fn(batch):
         en_batch, de_batch = zip(*batch)
         de_padded = pad_sequence(de_batch, batch_first=True, padding_value=pad_idx)
